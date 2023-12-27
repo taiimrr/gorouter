d9e4ae891af75dd92251654e1c797153620dec03
Taimoor <taimoor.nasir@mailbox.tu-dresden.de>
2023-12-27 14:14:52 +0100

    http server setup

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/taiimrr/gorouter
 
 go 1.21.5
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/go-chi/chi v1.5.5 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
--- main.go
+++ main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -14,6 +18,35 @@ func main(){
 	if portStr == ""{
 		log.Fatal("No port found")
 	}
+	
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+	
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+		v1Router.Get("/err", handlerErr)
+
+
+	router.Mount("/v1", v1Router)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr: ":" + portStr,
+	}
+
+	log.Printf("Server runnin on port %v", portStr)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println("Port:", portStr)
 
 }
